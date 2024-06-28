01e8bbe1212afaed5276980e7f8f10640d3acf99
Deniss-RO <denis808@smk.belajar.id>
2024-06-28 11:20:36 +0700

    adding detail khodam

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const fotoKhodam = [
-    {url: 'img/buayaPink.jpg', title: 'Buaya Pink'},
+    {url: 'img/buayaPink.jpg', title: 'Buaya Pink', detail: 'buaya pink berasal dari lautan di sekitar pulau permen'},
     {url: 'img/digidaw.jpg', title: 'Digidaw'},
     {url: 'img/esCendol.jpg', title: 'Es Cendol'},
     {url: 'img/gantunganBaju.jpg', title: 'Gantungan Baju'},
@@ -22,11 +22,12 @@ const fotoKhodam = [
     {url: 'img/tuyulPutih.jpg', title: 'Tuyul Putih'}
 ];
 
+const randomKhodam = Math.floor(Math.random() * fotoKhodam.length);
+const hasilKhodam = fotoKhodam[randomKhodam];
+const nama = document.getElementById('nama');
+const nama2 = nama.value;
+
 function randomizeKhodam() {
-    const randomKhodam = Math.floor(Math.random() * fotoKhodam.length);
-    const hasilKhodam = fotoKhodam[randomKhodam];
-    const nama = document.getElementById('nama');
-    const nama2 = nama.value;
 
     if(nama.value === '') {
         alert("Nama Tidak Boleh Kosong!!!");
@@ -35,11 +36,37 @@ function randomizeKhodam() {
         const imgKhodam = document.getElementById('img-khodam');
         imgKhodam.innerHTML = `
         <div id="txt-khodam">${nama2.toUpperCase()} Khodam Kamu Adalah</br> "${hasilKhodam.title}"</div>
-        <img src="${hasilKhodam.url}">
+        <img src="${hasilKhodam.url}"></br>
         `;
     }
-    nama.value= '';
+    nama.value= '';   
 }
+function openModal(){
+    const modal = document.getElementById('modal');
+    modal.style.display= 'block';
+    
+    const detailKhodam = document.getElementById('detail-khodam');
+    detailKhodam.innerHTML = `
+    <h1 id="txt-detail">${hasilKhodam.title}</h1>
+    `;
+
+}
+
+    // Close the modal when the user clicks on <span> (x)
+    document.querySelector('.close').addEventListener('click', function() {
+    document.getElementById('modal').style.display = 'none';
+});
+
+// Close the modal when the user clicks anywhere outside of the modal
+    window.addEventListener('click', function(event) {
+    const modal = document.getElementById('modal');
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+});
+
+    document.getElementById('btn-detail').addEventListener('click', openModal);
+    
     document.getElementById('btn-check').addEventListener('click', randomizeKhodam);
     document.getElementById('nama').addEventListener('keydown', function(event) {
         if (event.key === 'Enter') {
